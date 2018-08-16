bc7d1946496945eb4648ff0d368dd1d32c10bd6c
bjsmith3185 <bjsmith3185@hotmail.com>
2018-08-16 18:08:28 -0400

    modified the html in my files removing col-xs-4 leaving col-4,  copied the new html into logic.js template.

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -73,134 +73,110 @@ firebase.auth().onAuthStateChanged(function (user) {
       width: 2,
       height: 4,
       template: `<div class="container">
-    <div class="row">
-        <div class="col-xs-3"></div>
-        <div class="col-xs-6 text-center">
-            <div class="card">
-                <div class="card-title text-center" id="search-title">Top News Stories</div>
-                    <!-- search box popup -->
-                <div class="search-container">
-                    <div class="search-title text-center">Select a search topic</div>
-                        <div class="row-fluid">
-                            <div class="col-xs-6 text-center">
-                                <form id="form">
-                                    <select name="search" id="article-search">
-                                        <option value="world">world</option>
-                                        <option value="national">national</option>
-                                        <option value="politics">politics</option>
-                                        <option value="business">business</option>
-                                        <option value="technology">technology</option>
-                                        <option value="science">science</option>
-                                        <option value="Technology">Technology</option>
-                                        <option value="health">health</option>
-                                        <option value="fashion">fashion</option>
-                                        <option value="obituaries">obituaries</option>
-                                    </select>
-                                </form>
-                            </div>
-                            <div class="col-xs-1"></div>
-                            <div class="col-xs-5">
-                                <button class="btn btn-default" id="submit-article">Submit</button>
-                            </div>
-                    </div>    
-                </div>
-                    <!-- end of search popup -->
-                <div class="results-refresh-area">
-                    <div class="text-center" id="display-search-title"></div>
-                    <div class="display-top-stories" id="display-results"></div>
-                    <div class="card-body">
-                        <div class="refresh">Check for new stories</div>
-                        <div id="menu" class="glyphicon">&#xe236;</div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>`
+      <div class="row">
+          <div class="col-3"></div>
+          <div class="col-6 text-center">
+              <div class="card">
+                  <div class="card-title text-center" id="search-title">Top News Stories</div>
+                      <!-- search box popup -->
+                  <div class="search-container">
+                      <div class="search-title text-center">Select a search topic</div>
+                          <div class="row">
+                              <div class="col-6 text-center">
+                                  <form id="form">
+                                      <select name="search" id="article-search">
+                                          <option value="world">world</option>
+                                          <option value="national">national</option>
+                                          <option value="politics">politics</option>
+                                          <option value="business">business</option>
+                                          <option value="technology">technology</option>
+                                          <option value="science">science</option>
+                                          <option value="Technology">Technology</option>
+                                          <option value="health">health</option>
+                                          <option value="fashion">fashion</option>
+                                          <option value="obituaries">obituaries</option>
+                                      </select>
+                                  </form>
+                              </div>
+                              <div class="col-1"></div>
+                              <div class="col-5">
+                                  <button class="btn btn-default" id="submit-article">Submit</button>
+                              </div>
+                      </div>
+                         
+                  </div>
+                      <!-- end of search popup -->
+                  <div class="results-refresh-area">
+                      <div class="text-center" id="display-search-title"></div>
+                      <div class="display-top-stories" id="display-results"></div>
+                      <div class="card-body">
+                          <div class="refresh">Check for new stories</div>
+                          <div id="menu" class="glyphicon">&#xe236;</div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>`
     }
     var stockApp = {
       name: "stocks",
       width: 3,
       height: 4,
       template: `<div class="modal fade" id="stock-input-area" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
- <div class="modal-dialog" role="document">
-     <div class="modal-content">
-         <div class="modal-header">
-             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                 <span aria-hidden="true">&times;</span>
-             </button>
-
-             <h3 class="modal-title">Add a stock ticker symbol to your watchlist.</h3>
-             <h5 class="modal-title-sub">(Up to 5 stocks can be saved.)</h5>
-
-
-         </div>
-         <div class="modal-body">
-             <form>
-                 <span class="text-for-input">Enter Stock Symbol Here: </span>
-                 <input type="text" id="add-stock">
-
-             </form>
-         </div>
-         <div class="modal-footer">
-             <!-- <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button> -->
-             <button class="btn btn-default" data-dismiss="modal" id="submit-stock">Submit</button>
-         </div>
-     </div>
- </div>
-</div>
-
-
-
+      <div class="modal-dialog" role="document">
+          <div class="modal-content">
+              <div class="modal-header">
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                      <span aria-hidden="true">&times;</span>
+                  </button>
+                  <h3 class="modal-title">Add a stock ticker symbol to your watchlist.</h3>
+                  <h5 class="modal-title-sub">(Up to 5 stocks can be saved.)</h5>
+              </div>
+              <div class="modal-body">
+                  <form>
+                      <span class="text-for-input">Enter Stock Symbol Here: </span>
+                      <input type="text" id="add-stock">
+                    </form>
+              </div>
+              <div class="modal-footer">
+                  <button class="btn btn-default" data-dismiss="modal" id="submit-stock">Submit</button>
+              </div>
+          </div>
+      </div>
+  </div>
 <!-- widget area  -->
-
 <div class="container">
-<div class="row">
- <div class="col-xs-4"></div>
- <div class="col-xs-4">
-
-         <div class="stock-widget-container">
-         
-             <!-- <div id="menu"><span class="glyphicon">&#xe236;</span></div> -->
-             <div id="menu" data-toggle="modal" data-target="#stock-input-area"><span class="glyphicon">&#xe236;</span></div>
-         
-            
-
-             <div class="card text-center" >
-                     
-                     <div class="card-body text-center">
-                         <div class="card-title">Your Favorite Stocks</div>
-
-                         <div class="card-text stock stock-0">
-                             <span id="stock-0-name"></span>
-                             <span id="stock-0-data"></span>
-                             <span id="stock-0-change"></span>
-                         </div>
-
-                         <div class="card-text stock stock-1">
-                             <span id="stock-1-name"></span>
-                             <span id="stock-1-data"></span>
-                             <span id="stock-1-change"></span>
-                         </div>
-
-                         <div class="card-text stock stock-2">
-                             <span id="stock-2-name"></span>
-                             <span id="stock-2-data"></span>
-                             <span id="stock-2-change"></span>
-                         </div>
-
-
-
-                   </div>
-         
-         
-         </div>
-         <div class="refresh-area text-center" id="click-me">Click to refresh</div>
-         </div>
-
- </div>
- <div class="col-xs-4"></div>
-</div>
+  <div class="row">
+      <div class="col-4"></div>
+      <div class="col-4">
+              <div class="stock-widget-container">
+                  <div id="menu" data-toggle="modal" data-target="#stock-input-area"><span class="glyphicon">&#xe236;</span></div>
+                  <div class="card text-center" >
+                          <div class="card-body text-center">
+                              <div class="card-title">Your Favorite Stocks</div>
+                              <div class="card-text stock stock-0">
+                                  <span id="stock-0-name"></span>
+                                  <span id="stock-0-data"></span>
+                                  <span id="stock-0-change"></span>
+                              </div>
+                              <div class="card-text stock stock-1">
+                                  <span id="stock-1-name"></span>
+                                  <span id="stock-1-data"></span>
+                                  <span id="stock-1-change"></span>
+                              </div>
+                              <div class="card-text stock stock-2">
+                                  <span id="stock-2-name"></span>
+                                  <span id="stock-2-data"></span>
+                                  <span id="stock-2-change"></span>
+                              </div>
+                        </div>
+              </div>
+              <div class="refresh-area text-center" id="click-me">Click to refresh</div>
+              </div>
+      </div>
+      <div class="col-4"></div>
+  </div>
 </div>`
     }
     var todoApp={
@@ -208,33 +184,31 @@ firebase.auth().onAuthStateChanged(function (user) {
       width: 2,
       height: 4,
       template:`<div class="row">
-      <div class="col-xs-12">
+      <div class="col-12">
           <div class="panel panel-default" id="panel-list-body">
               <div class="panel-heading" id="heading-box">
                   <h1 class="panel-title" id="title"><span id="title-1">Get It Done List,</span><span id="title-2"> powered by firebase.</span></h1>
               </div>
               <div class="panel-body" id="input-body">
                   <div class="row" id="top-row">
-                      <div class="col-xs-3 box">
+                      <div class="col-3 box">
                           <div id="store-title">Task</div>
                       </div>
-                      <div class="col-xs-3 box">
+                      <div class="col-3 box">
                           <div id="item-title">Notes</div>
                       </div>
-                      <div class="col-xs-3 box"></div>
-                      <div class="col-xs-3 box"></div>
-  
+                      <div class="col-3 box"></div>
+                      <div class="col-3 box"></div>
                   </div>
-  
                   <div class="row" id="bottom-row">
-                      <div class="col-xs-3 box">
+                      <div class="col-3 box">
                           <input type="text" id="task-input" placeholder=" Enter Task">
                       </div>
-                      <div class="col-xs-3 box">
+                      <div class="col-3 box">
                           <input type="text" id="note-input" placeholder=" Enter notes">
                       </div>
-                      <div class="col-xs-3 box"></div>
-                      <div class="col-xs-3 box">
+                      <div class="col-3 box"></div>
+                      <div class="col-3 box">
                           <button class="w3-button w3-border w3-xlarge" id="submit">Submit <span class="glyphicon glyphicon-ok" aria-hidden="true"></span></button>
                       </div>
                   </div>
@@ -242,15 +216,13 @@ firebase.auth().onAuthStateChanged(function (user) {
           </div>
       </div>
   </div>
-  
-  
   <div class="row">
-      <div class="col-xs-1"></div>
-      <div class="col-xs-10">
+      <div class="col-1"></div>
+      <div class="col-10">
           <p id="list-heading">Things To Do....</p>
           <div id="list"></div>
       </div>
-      <div class="col-xs-1"></div>
+      <div class="col-1"></div>
   </div>`
     }
     var chatApp= {
