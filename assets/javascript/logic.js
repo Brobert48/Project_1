b09e7a964a9c286d88567055f83845e019800ff5
Blake Robert <blaker.blaker4@gmail.com>
2018-08-15 23:04:09 -0400

    widgets populate to the grid dynamically based on the user's preferences

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,5 +1,5 @@
 var database = firebase.database();
-var currentx =0;
+var currentx = 0;
 firebase.auth().onAuthStateChanged(function (user) {
   if (user) {
     var uid = firebase.auth().currentUser.uid;
@@ -47,7 +47,7 @@ firebase.auth().onAuthStateChanged(function (user) {
     userNameH5.attr('class', 'navbar-text')
       .text(firebase.auth().currentUser.displayName);
     userNameLi.append(userNameH5);
-    
+
     targetDiv.append(userNameH5);
     // Logout navbar link
     var logoutLi = $('<li>');
@@ -61,72 +61,58 @@ firebase.auth().onAuthStateChanged(function (user) {
     targetDiv.append(logoutLi);
 
 
-  
-    // Populate Widgets
-    let populateWidgets = function (widgetName, widgetW, widgetH, currentX, widgetContent){
-    TargetDiv = $('#widgetArea')
-    widgetContainer = $('<div>')
-    widgetContainer.attr('class','grid-stack')
-    .attr('id', widgetName) //change based on widget name
-    widgetlocation = $('<div>')
-    widgetlocation.attr('class','grid-stack-item')
-    .attr('data-gs-x', currentX) //location on grid x-axis
-    .attr('data-gs-y','0') //location on grid y-axis
-    .attr('data-gs-width', widgetW) //width of widget
-    .attr('data-gs-height', widgetH) //height of widget
-    lastwidgetLayer = $('<div>')
-    .attr('class','grid-stack-item-content card float-left border border-primary')
-    TargetDiv.prepend(widgetContainer);
-    widgetContainer.append(widgetlocation);
-    widgetlocation.append(lastwidgetLayer);
-    lastwidgetLayer.append(widgetContent);
-   currentx = currentx + widgetW + 1;
-  }
 
-  // weatherOBJ
-  var weatherApp={
-  name: "weather",
-  text1: 'todays weather highlights',
-  text2: 'Full Weather',
-  width: 4,
-  height: 4}
-  weatherTemplate = `<div class="card float-left">
-   <img class="card-img-top" id="weather-card-img" src="http://via.placeholder.com/150x150" alt="Card image cap">
+    // Populate Widgets
+    let populateWidgets = function (widgetName, widgetW, widgetH, currentX, widgetContent) {
+      var Target = $('#widgetArea')
+      var widget = `<div class="grid-stack-item" id="${widgetName}" data-gs-x="${currentX}" data-gs-y="0" data-gs-width="${widgetW}" data-gs-height="${widgetH}"><div class="grid-stack-item-content card float-left border border-primary">${widgetContent}</div></div>`
+      Target.append(widget);
+      // widgetContainer = $('<div>')    // widgetContainer.attr('class','grid-stack')    // .attr('id', widgetName) //change based on widget name    // widgetlocation = $('<div>')    // widgetlocation.attr('class','grid-stack-item')    // .attr('data-gs-x', currentX) //location on grid x-axis    // .attr('data-gs-y','0') //location on grid y-axis    // .attr('data-gs-width', widgetW) //width of widget    // .attr('data-gs-height', widgetH) //height of widget    // lastwidgetLayer = $('<div>')    // .attr('class','grid-stack-item-content card float-left border border-primary')    // TargetDiv.prepend(widgetContainer);    // widgetContainer.append(widgetlocation);    // widgetlocation.append(lastwidgetLayer);    // lastwidgetLayer.append(widgetContent);
+      currentx = currentX + widgetW;
+    }
+
+    // weatherOBJ
+    var weatherApp = {
+      name: "weather",
+      text1: 'todays weather highlights',
+      text2: 'Full Weather',
+      width: 2,
+      height: 4
+    }
+    weatherTemplate = `<div class="card float-left">
+   <img class="card-img-top" id="weather-card-img" src="" alt="Card image cap">
    <div class="card-body">
        <h5 class="card-title">${weatherApp.name}</h5>
        <p class="card-text" id="weather-card-text">${weatherApp.text1}</p>
        <a href="#" class="btn btn-primary" id="weather-button">${weatherApp.text2}</a>
    </div>
 </div>`
-  
 
-  // todo list
 
-  let populatetodo = function (){
 
-  }
-  
-  // weather
-  
-  let populateWeather = function(){
-
-  }
-
-  // stock ticker check
-
-  // if(database.ref('/users').child(uid).child('widgets').child('weather').child('active').val() === "on"){
-    populateWidgets(weatherApp.name, weatherApp.width, weatherApp.height, currentx, weatherTemplate);
-
-  
-
-  // todo list check
-
-  // weather check
-
-  // news
-
-  // notes
-
+    database.ref().once('value').then(function (childsnap) {
+      // weather check
+      if (childsnap.child('users').child(uid).child('widgets').child('weather').child('active').val() === "on") {
+        populateWidgets(weatherApp.name, weatherApp.width, weatherApp.height, currentx, weatherTemplate);
+      }
+      // stock check
+      if (childsnap.child('users').child(uid).child('widgets').child('stocks').child('active').val() === "on") {
+        populateWidgets(weatherApp.name, weatherApp.width, weatherApp.height, currentx, weatherTemplate);
+      }
+      // todo check
+      if (childsnap.child('users').child(uid).child('widgets').child('todo').child('active').val() === "on") {
+        populateWidgets(weatherApp.name, weatherApp.width, weatherApp.height, currentx, weatherTemplate);
+      }
+      // news check
+      if (childsnap.child('users').child(uid).child('widgets').child('news').child('active').val() === "on") {
+        populateWidgets(weatherApp.name, weatherApp.width, weatherApp.height, currentx, weatherTemplate);
+      }
+      // notes check
+      if (childsnap.child('users').child(uid).child('widgets').child('notes').child('active').val() === "on") {
+        populateWidgets(weatherApp.name, weatherApp.width, weatherApp.height, currentx, weatherTemplate);
+      }
+      gridstackConfig();
+    })
 
   }
   else {
@@ -134,12 +120,11 @@ firebase.auth().onAuthStateChanged(function (user) {
   }
 });
 
-let gridstackConfig = function (){
+let gridstackConfig = function () {
   console.log('ran')
   var options = {
     cellHeight: 80,
     verticalMargin: 10
-};
-$('.grid-stack').gridstack(options);
+  };
+  $('.grid-stack').gridstack(options);
 }
-setTimeout(gridstackConfig,1000 *1);
