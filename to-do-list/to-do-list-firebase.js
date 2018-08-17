102b9e153905f3cb3f134bd64513a036c78ecdb2
bjsmith3185 <bjsmith3185@hotmail.com>
2018-08-17 16:05:30 -0400

    the firebase config was commented out on stock app, corrected this.

--- to-do-list/to-do-list-firebase.js
+++ to-do-list/to-do-list-firebase.js
@@ -1,15 +1,15 @@
 
 
-// var config = {
-//     apiKey: "AIzaSyCiPgGOcvsJ0Ws54KMX1p0mCia3a1hJ2UI",
-//     authDomain: "project-1-firebase-1b2fb.firebaseapp.com",
-//     databaseURL: "https://project-1-firebase-1b2fb.firebaseio.com",
-//     projectId: "project-1-firebase-1b2fb",
-//     storageBucket: "project-1-firebase-1b2fb.appspot.com",
-//     messagingSenderId: "693404188715"
-// };
+var config = {
+    apiKey: "AIzaSyCiPgGOcvsJ0Ws54KMX1p0mCia3a1hJ2UI",
+    authDomain: "project-1-firebase-1b2fb.firebaseapp.com",
+    databaseURL: "https://project-1-firebase-1b2fb.firebaseio.com",
+    projectId: "project-1-firebase-1b2fb",
+    storageBucket: "project-1-firebase-1b2fb.appspot.com",
+    messagingSenderId: "693404188715"
+};
 
-// firebase.initializeApp(config);
+firebase.initializeApp(config);
 
 $(document).ready(function () {
     var database = firebase.database();
