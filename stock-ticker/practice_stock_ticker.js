49b42a96d0d3e1c70726243a9725843b10486760
Blake Robert <Blaker.blaker4@gmail.com>
2018-08-18 11:26:00 -0400

    functioning Stocks app

--- stock-ticker/practice_stock_ticker.js
+++ stock-ticker/practice_stock_ticker.js
@@ -1,20 +1,20 @@
 
 
-var config = {
-    apiKey: "AIzaSyCiPgGOcvsJ0Ws54KMX1p0mCia3a1hJ2UI",
-    authDomain: "project-1-firebase-1b2fb.firebaseapp.com",
-    databaseURL: "https://project-1-firebase-1b2fb.firebaseio.com",
-    projectId: "project-1-firebase-1b2fb",
-    storageBucket: "project-1-firebase-1b2fb.appspot.com",
-    messagingSenderId: "693404188715"
-};
+// var config = {
+//     apiKey: "AIzaSyCiPgGOcvsJ0Ws54KMX1p0mCia3a1hJ2UI",
+//     authDomain: "project-1-firebase-1b2fb.firebaseapp.com",
+//     databaseURL: "https://project-1-firebase-1b2fb.firebaseio.com",
+//     projectId: "project-1-firebase-1b2fb",
+//     storageBucket: "project-1-firebase-1b2fb.appspot.com",
+//     messagingSenderId: "693404188715"
+// };
 
-  firebase.initializeApp(config);
+//   firebase.initializeApp(config);
   var database = firebase.database();
 
 
 $( document ).ready(function() {
-    $(".stock-input-container").hide();
+//     $(".stock-input-container").hide();
 
 
 // button to open window to add new stock
@@ -144,7 +144,7 @@ function getFromDatabase() {
 
 // this click will refresh the page
 $("#click-me").on("click", function () {
-    alert("refreshing");
+    event.preventDefault();
     getFromDatabase();
 });
 
